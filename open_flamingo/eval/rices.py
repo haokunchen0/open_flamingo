6ba7df0a161f47452a194bc4bf8021b918620d95
haokunchen0 <haokunchen0@gmail.com>
2023-09-12 14:52:13 +0800

    支持文本rice以及新的git忽略

--- open_flamingo/eval/rices.py
+++ open_flamingo/eval/rices.py
@@ -3,7 +3,7 @@ import torch
 from tqdm import tqdm
 import torch
 from utils import custom_collate_fn
-
+import random
 
 class RICES:
     def __init__(
@@ -11,7 +11,7 @@ class RICES:
         dataset,
         device,
         batch_size,
-        vision_encoder_path="ViT-B-32",
+        vision_encoder_path="ViT-L-14",
         vision_encoder_pretrained="openai",
         cached_features=None,
     ):
@@ -93,3 +93,98 @@ class RICES:
 
         # Return with the most similar images last
         return [[self.dataset[i] for i in reversed(row)] for row in indices]
+
+
+class RICES_Text:
+    def __init__(
+        self,
+        dataset,
+        text_label,
+        device,
+        batch_size,
+        vision_encoder_path="ViT-L-14",
+        vision_encoder_pretrained="openai",
+        cached_features=None,
+    ):
+        self.dataset = dataset
+        self.text_label = text_label
+        self.device = device
+        self.batch_size = batch_size
+
+        # Load the model and tokenizer
+        self.model, _, self.image_processor = open_clip.create_model_and_transforms(
+            vision_encoder_path,
+            pretrained=vision_encoder_pretrained,
+        )
+        self.model = self.model.to(self.device)
+        self.tokenizer = open_clip.get_tokenizer(vision_encoder_path)
+
+        # Tokenize the text descriptions and move to device
+        self.text_tokens = self.tokenizer(text_label).to(self.device)
+
+        # Precompute features
+        if cached_features is None:
+            self.text_features = self._precompute_text_features()
+        else:
+            self.text_features = cached_features
+
+    def _precompute_text_features(self):
+        # Switch to evaluation mode
+        self.model.eval()
+
+        with torch.no_grad():
+            text_features = self.model.encode_text(self.text_tokens)
+            text_features /= text_features.norm(dim=-1, keepdim=True)
+        return text_features.detach().cpu()
+
+    def find(self, batch, num_examples):
+        """
+        Get the ranked text descriptions based on similarity to the image.
+        """
+        # Switch to evaluation mode
+        self.model.eval()
+
+        with torch.no_grad():
+            # Preprocess and encode the image
+            image_input = torch.stack([self.image_processor(image) for image in batch]).to(
+                self.device
+            )
+            #Get the feature of the input image
+            query_feature = self.model.encode_image(image_input)
+            query_feature /= query_feature.norm(dim=-1, keepdim=True)
+            query_feature = query_feature.detach().cpu()
+
+            if query_feature.ndim == 1:
+                query_feature = query_feature.unsqueeze(0)
+            # Compute the similarity between image and text representations
+            similarities = (query_feature @ self.text_features.T).squeeze()
+            if similarities.ndim == 1:
+                similarities = similarities.unsqueeze(0)
+            
+            # Get the indices of the 'num_examples' most similar texts
+            indices = similarities.argsort(dim=-1, descending=True)[:, :num_examples]
+        # Return the most similar text descriptions
+        return [[self.text_label[i] for i in row] for row in indices]
+    
+    def get_images_from_labels(self, batch_labels, shot):
+        """
+        Get 'shot' number of images for each label in the batch from the training dataset.
+        """
+        results = []
+        for label_list in batch_labels:
+            label = label_list[0]  # As per your example, there's one label per batch item
+            matching_indices = [
+                idx for idx, sample in enumerate(self.dataset.imgs) if self.dataset.class_id_to_name[sample[1]] == label
+            ]
+            random.seed(42)
+            selected_indices = random.sample(matching_indices, shot)
+            selected_samples = [self.dataset[idx] for idx in selected_indices]
+            results.append(selected_samples)
+        return results
+
+
+
+
+
+
+
